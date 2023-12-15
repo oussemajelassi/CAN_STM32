0c0328356888064494969d3270351d436ec90bf7
oussemajelassi <oussjls57@gmail.com>
2023-12-15 19:50:15 +0100

    Initial Commit, LoopBack recieves message now

---
+++ CAN_LoopBack/CM7/Core/Inc/CAN_Communication.h
@@ -0,0 +1,13 @@
+/*
+ * CAN_Communication.h
+ *
+ *  Created on: Dec 15, 2023
+ *      Author: ousjl
+ */
+
+#ifndef INC_CAN_COMMUNICATION_H_
+#define INC_CAN_COMMUNICATION_H_
+
+void vCAN_FilterInit ( void ) ;
+
+#endif /* INC_CAN_COMMUNICATION_H_ */
